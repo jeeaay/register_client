a320eda64970bb3975e2a7c4a75d221730173d5b
jeay <wrjie@msn.cn>
2022-04-25 17:03:23 +0800

    添加注册进程

---
+++ reg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+'''
+Email: wrj7887@163.com
+Author: Jeay
+Date: 2022-04-25 15:36:42
+LastEditors: Jeay
+LastEditTime: 2022-04-25 15:38:00
+jeay.net
+FilePath: \client\reg.py
+Description: 调用注册程序
+Copyright (c) 2022 by jeay.net, All Rights Reserved.
+'''
+
+from register import DomainCore
+if __name__ == "__main__":
+    dc = DomainCore()
+    dc.MainController()
